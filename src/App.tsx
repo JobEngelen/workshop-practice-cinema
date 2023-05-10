720bf5ed23ac3c79391918bab529efa1333b8dbe
JobEngelen <jobengelen@yahoo.nl>
2023-05-10 15:17:40 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,123 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IMovie {
+  id: number;
+  name: string;
+  genre: string;
+  rating: number;
+  releasedate: string;
 }
 
-export default App;
+export default function App() {
+  const [editId, setEditId] = useState<number | null>(null);
+  const [newMovieName, setNewMovieName] = useState<string>('');
+
+  const getMovies = async () => {
+    const movies: IMovie[] = [];
+
+    await fetch('/movies.csv')
+      .then(response => response.text()
+      )
+      .then(data => {
+        const rows = data.split('\n').slice(1);
+        rows.forEach(row => {
+          const columns = row.split(',');
+          const movie: IMovie = {
+            id: parseInt(columns[0]),
+            name: columns[1],
+            genre: columns[2],
+            rating: parseInt(columns[3]),
+            releasedate: columns[4]
+          }
+          movies.push(movie);
+        })
+      });
+
+    return movies;
+  }
+
+  const [movies, setMovies] = useState<IMovie[]>([]);
+
+
+
+  useEffect(() => {
+    const getProps = async () => {
+      setMovies(await getMovies());
+    }
+    getProps();
+  }, []);
+
+
+  const deleteMovie = async (id: number) => {
+    const newMovies = movies.filter(movie => movie.id !== id);
+    setMovies(newMovies);
+  }
+
+  const handleEditPress = (id: number) => {
+    // Set the editId state to the given ID
+    setEditId(editId === id ? null : id);
+    setNewMovieName(movies.find(movie => movie.id === id)?.name || '');
+  }
+
+  const saveMovie = async (id: number) => {
+    const newMovies = movies.map(movie => {
+      if (movie.id === id) {
+        movie.name = newMovieName;
+      }
+      return movie;
+    });
+    setMovies(newMovies);
+    setEditId(null);
+  }
+
+
+  return (
+    <main className="main">
+      <div className="center">
+        <h1 className="title"> cinema </h1>
+      </div>
+
+      <div className="center">
+        <div className="grid">
+          {movies.length > 0 &&
+            movies.map((movie: IMovie) => {
+              return (
+                <div className="card" key={movie.id}>
+                  <h3>{movie.name}</h3>
+                  <p>Genre: {movie.genre}</p>
+                  <p>Rating: {movie.rating}</p>
+                  <p>Release: {movie.releasedate}</p>
+
+                  <button
+                    className="editButton"
+                    onClick={() => handleEditPress(movie.id)}
+                  >
+                    Edit
+                  </button>
+
+                  <button
+                    className="deleteButton"
+                    onClick={() => deleteMovie(movie.id)}
+                  >
+                    Delete
+                  </button>
+
+                  <div className="edit"
+                    hidden={editId !== movie.id}
+                  >
+                    <input type="text" value={newMovieName} onChange={(e) => setNewMovieName(e.target.value)} />
+                    <button
+                      onClick={() => saveMovie(movie.id)}
+                    >
+                      Save
+                    </button>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div >
+      </div>
+    </main>
+  )
+}
